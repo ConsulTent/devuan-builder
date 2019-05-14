f726d2004c74f65e405d634375eaa5a5faa0b5e6
azilber <azilber@consultent.ltd>
2019-05-14 21:58:31 +0800

    Azure Devuan Builder

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# devuan-builder for ECS
+# devuan-builder for Microsoft Azure
 Reproducible build system for Devuan using Packer, Virtualbox and public cloud.
 
 ## Pre-requisites (OSX specific)
 
-* [ossutil tools](https://www.alibabacloud.com/help/doc-detail/50452.htm) (download and place in tools/)
+* [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/?view=azure-cli-latest) (brew install azure-cli)
 * [Oracle Virtualbox](https://www.virtualbox.org/)
 * [HashiCorp Packer](https://packer.io/guides/)
 * [brew](https://brew.sh/)
@@ -13,22 +13,28 @@ Reproducible build system for Devuan using Packer, Virtualbox and public cloud.
 
 
 
-**You're on the Devuan 2 (Ascii) Branch for Alibaba Cloud ECS**
+**You're on the Devuan 2 (Ascii) Branch for Microsoft Azure**
 
-Make sure everything above is installed and your AliBaba Cloud account credentials are configured in ossutil.  Example:
-./ossutilmac64 config -e oss-ap-southeast-3.aliyuncs.com -i "access-key-id" -k "access-key-secret"
+**This will:**
+
+1. Use VirtualBox to build a base image w/ cloud-init.
+2. Package it up as a VHD
+3. Upload it to your Azure Storage Accounts' Container.
+4. Register the the blob as an Image.
+
+Make sure everything above is installed and your Azure CLI is configured as per *az login*.
 
 build-devuan.sh will help you set that up.
 
-Retrieve the endpoint data from the OSS console.
+Setup a Storage Account of 'StorageV2', then create a container within it.  Edit
+STORAGEACCOUNT and CONTAINER in build-devuan.sh.
 
-
-  Edit build-devuan.sh and change variables as needed.  You don't have to, but you might want to (such as changing the mirror where you're downloading the ISO from).
+Change any other variables as needed.  You don't have to, but you might want to (such as changing the mirror where you're downloading the ISO from).
 
 Then run:  ./build-devuan.sh
 
 Packer will download the Install ISO and build the image using VirtualBox.
-It will export the VM to OVA, convert to VHD then upload the VHD to your OSS bucket, then import the snapshot.
+It will export the VM to OVA, convert to VHD then upload the VHD to your Storage container as a blob, then import the snapshot.
 It will be up to you to monitor the conversion (example output will be provided).  You can then use the console (easiest) to convert the snapshot into an AMI.
 
-Use your public key to ssh in as `admin@<instance ip>`
+Use a public key in the dashboard with a username of your choice to ssh in.
--- build-devuan.sh
+++ build-devuan.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+VERSION='1.0'
+
 # Change these:
 
 # HNAME is the hostname/snapshot name, etc.
 HNAME='devuan-ascii'
-VERSION='1.0'
+INAME='Devuan_Ascii_2GB_base'
+# 2000 = 2GB
+DISK_SIZE=2000
 
 ISO='http://devuan.c3l.lu/devuan_ascii/installer-iso/devuan_ascii_2.0.0_amd64_netinst.iso'
 ISO_SHA256=c16bebdeecdf452188ae4bb823cd5f1c0d2ed3a7a568332508943ce16f7e5c71
 # Target upload bucket
-OSS_BUCKET='consultent-ecs'
+STORAGEACCOUNT='consultentfiles'
+CONTAINER='devuan'
 DESCRIPTION='A Devuan Ascii build by Somebody'
+RESOURCEGROUP='ConsulTent_Dev'
 
 
 ####################################### DON'T CHANGE BELOW ##############################3
 
 #Pre-requisites check
-CMDS=( aws tools/ossutil* VBoxManage packer )
+CMDS=( az VBoxManage packer )
 for CMD in "${CMDS[@]}"; do
   CHK_CMD=`command -v ${CMD}`
   if [ -z "${CHK_CMD}" ]; then
@@ -25,40 +31,24 @@ for CMD in "${CMDS[@]}"; do
   fi
 done
 
-OSSCMD=`command -v tools/ossutil*`
+AZCMD=`command -v az`
 
-if [ ! -f ~/.ossutilconfig ]; then
-  echo "No OSS config file found! We'll try to configure it all now for you."
-  echo "Make sure you've created an OSS bucket here: https://oss.console.aliyun.com/bucket/"
-  echo "You will need data from there..."
-  echo -n "Enter your AccessKey ID: "
-    read ACCESSKEYID
-  echo " "; echo -n "Enter your Access Key Secret: "
-    read ACCESSKEYSECRET
-  echo " "; echo -n "Enter your enpoint url.  Example: 'ap-southeast-3': "
-    read OSSENDPOINT
-  echo " "
-
-OSSCMD config -e $OSSENDPOINT -i "${ACCESSKEYID}" -k "${ACCESSKEYSECRET}"
-mkdir -p ~/.ecs
-cat << EOF >> ~/.ecs/credentials
-ALICLOUD_ACCESS_KEY="${ACCESSKEYID}"
-ALICLOUD_SECRET_KEY="${ACCESSKEYSECRET}"
-ALICLOUD_REGION="$OSSENDPOINT"
-EOF
-chmod 600 ~/.ecs/credentials
+if [ ! -d ~/.azure ]; then
+  echo "No Azure config file found! We'll try to configure it all now for you."
+az --login
 
 fi
 
-if [ ! -f ~/.ecs/credentials ]; then
-  echo "ERROR: Please configure ECS credentials in ~/.ecs/credentials according to https://www.packer.io/docs/post-processors/alicloud-import.html"
-  exit
+STORAGEKEY=$($AZCMD storage account keys list --account-name ${STORAGEACCOUNT} --query "[0].value" | tr -d '"')
+
+if [ -z "${STORAGEKEY}" ]; then
+echo "No storage keys found, please create storage account with generic storage, and a blob container."
+exit
 fi
+
 
 JQX=`command -v jq`
 ####
-
-source ~/.ecs/credentials
 
 echo "Devuan Ascii Builder v${VERSION}"
 
@@ -67,7 +57,7 @@ echo "ERROR: output-virtualbox-iso directory exits and will break the build. Ple
 exit
 fi
 
-packer build -on-error=ask -var "iso=${ISO}" -var "isosha256=${ISO_SHA256}" -var "hostname=${HNAME}" -var "build_version=${VERSION}" -var "vm_description=${DESCRIPTION}" -var "access_key=${ALICLOUD_ACCESS_KEY}" -var "secret_key=${ALICLOUD_SECRET_KEY}" -var "oss_bucket_name=${OSS_BUCKET}" -var "region=${ALICLOUD_REGION}" devuan-base.ecs.json
+packer build -on-error=ask -var "disk_size=${DISK_SIZE}" -var "iso=${ISO}" -var "isosha256=${ISO_SHA256}" -var "hostname=${HNAME}" -var "build_version=${VERSION}" -var "vm_description=${DESCRIPTION}" devuan-base.vm.json
 
 if [ ! -d output-virtualbox-iso ]; then
 echo "ERROR: Director output-virtualbox-iso not found, something went wrong."
@@ -92,13 +82,18 @@ UUID=`VBoxManage list hdds |grep -B4 "virtualbox-${HNAME}.vhd" |head -1|cut -d: 
 fi
 
 if [ -f "output-virtualbox-iso/virtualbox-${HNAME}.vhd" ]; then
-${OSSCMD} cp output-virtualbox-iso/virtualbox-${HNAME}.vhd oss://${OSS_BUCKET}/
+#${AZCMD} storage blob upload --container-name ${CONTAINER} --file output-virtualbox-iso/virtualbox-${HNAME}.vhd --name ${HNAME}.vhd
+#${AZCMD} storage file upload --account-name ${STORAGEACCOUNT} --account-key "${STORAGEKEY}" --source "output-virtualbox-iso/virtualbox-${HNAME}.vhd" --path "${HNAME}.vhd" -s "${CONTAINER}"
+${AZCMD} storage blob upload --container-name "${CONTAINER}" --account-name "${STORAGEACCOUNT}" --name "${HNAME}.vhd" --file "output-virtualbox-iso/virtualbox-${HNAME}.vhd" --type page
+${AZCMD} image create --os-type Linux --source https://${STORAGEACCOUNT}.blob.core.windows.net/${CONTAINER}/${HNAME}.vhd --name "${INAME}" -g ${RESOURCEGROUP} --tags packer=true
+
 else
-echo "ERROR: Skipping uploading of output-virtualbox-iso/${convert_file} to oss://${OSS_BUCKET}/, something went wrong."
+echo "ERROR: Skipping uploading of output-virtualbox-iso/virtualbox-${HNAME}.vhd to https://${STORAGEACCOUNT}.blob.core.windows.net/${CONTAINER}/, something went wrong."
 exit
 fi
 
-echo "Go to the Aliyun ECS console and import the custom image"
+echo "Go to the Azure console and check on your image status."
+echo "https://portal.azure.com/#blade/HubsExtension/BrowseResourceBlade/resourceType/Microsoft.Compute%2Fimages"
 
 #Uncomment/Comment the line below if you don't/do want to remove the output directory...
-rm -Rf output-virtualbox-iso/
+#rm -Rf output-virtualbox-iso/
